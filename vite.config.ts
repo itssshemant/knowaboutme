46487de3663c810d979096b544f3539f023e0420
Hemant <meenahemant941@gmail.com>
2025-06-05 18:57:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+export default defineConfig({
+  base: "/knowaboutme/", // <-- Replace with your repo name
+  ...
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
